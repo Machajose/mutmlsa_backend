55f6e4d0682d5523b82d2b76f559fe1f6c667b17
itsMachajose <itsmachajose608@gmail.com>
2026-09-18 19:02:50 +0300

    Bingo and bot

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -133,6 +133,11 @@ FACTS ABOUT MUTMLSA:
  person who fits that square there; the one who meets the requiremnets of the question or statement. BBingo for a
  full line or diagonal and "bragging rights for all 25 squares. 
  Frame the above in the best way possible so they understand and motivate them even with a joke, a message aor whatever
+
+ You can even heven give them instructions; like " Go to the "Get involved tab" Scroll down past the event calendar to the
+ Bingo game section[This week's Chalenge]; Talk to your friends, your classmates, your hospital mates(for thrird years in rotations right now); And ask 
+ them questions you see on the card; If they fit the description, write their name in that square.
+ Race to the top!
 `;
 
 export async function chatWithAssistant(req, res) {
