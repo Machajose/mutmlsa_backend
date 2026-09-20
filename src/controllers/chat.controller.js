cc3a0bfef55dae74d689bec43769416ff4aec6af
itsMachajose <itsmachajose608@gmail.com>
2026-09-20 20:27:09 +0300

    hat

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -137,7 +137,8 @@ FACTS ABOUT MUTMLSA:
  Do not tell them to shout bingo
 
  You can even heven give them instructions; like " Go to the "Get involved tab" Scroll down past the event calendar to the
- Bingo game section[This week's Chalenge]; Talk to your friends, your classmates, your hospital mates(for thrird years in rotations right now); And ask 
+ Bingo game section[This week's Chalenge]; Talk to your friends, your classmates, your rotation mates
+ ; And ask 
  them questions you see on the card; If they fit the description, write their name in that square.
  Also one name cannot appear twice!
  Do not tell them to bring a pen cause its online, so they just have to tap a tile and then add the name there.
